05fa8136ba7afc3ff912920a79c3c43f8663591b
Hiên <hien798@gmail.com>
2019-08-03 21:02:27 +0700

    Updates

---
+++ precache-manifest.aeeced16094b779e4e9e8e9e2cb5cec4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf0b0ccf0ead789ed9afd259510ac25",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "d0fc3ee8895fd529558a",
+    "url": "/resume/static/css/main.d7394254.chunk.css"
+  },
+  {
+    "revision": "3a8cc4d7bf37e66324e4",
+    "url": "/resume/static/js/2.d68f1bfa.chunk.js"
+  },
+  {
+    "revision": "d0fc3ee8895fd529558a",
+    "url": "/resume/static/js/main.71e0f7ae.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  }
+]);
